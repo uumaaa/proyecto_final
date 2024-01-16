2460481f6e1aab5ed439f0da006003161d151189
uumaaa <vma.iantonio@gmail.com>
2024-01-16 13:00:44 -0600

    señales

--- modificador_peach.py
+++ modificador_peach.py
@@ -40,8 +40,8 @@ def generate_pitch_variations(input_file, output_folder, csv_file,message,respon
         append_to_csv(csv_file, [output_file_name,message,response])
 
 if __name__ == "__main__":
-    input_audio_file = 'audios/holad.wav'
+    input_audio_file = 'audios/chisteg.wav'
     output_folder = "audios"
     csv_file = "data.csv"
 
-    generate_pitch_variations(input_audio_file, output_folder, csv_file, '"HOLA"','"HOLA"',num_variations=200)
+    generate_pitch_variations(input_audio_file, output_folder, csv_file, '"CUENTAME UN CHISTE"','"¿COMO SE LLAMA UN PEZ QUE SABE PROGRAMAR? CODFISH"',num_variations=200)
